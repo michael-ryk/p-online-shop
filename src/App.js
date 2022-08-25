366bd588d4f1da7550362e2bbdac7fdb8273b6e3
MichaelR <spider747@gmail.com>
2022-08-25 18:48:07 +0300

    Create blank pages with routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import Products from './pages/Products';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import NotFound from './pages/NotFound';
+
 import styled from 'styled-components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Online Shop</h1>
-        <h1>Test</h1>
+    <div>
+      <Routes>
+        <Route path='/' element={<Navigate replace to='/homepage' />} />
+        <Route path='homepage' element={<HomePage />} />
+        <Route path='products' element={<Products />} />
+        <Route path='about' element={<About />} />
+        <Route path='contacts' element={<Contacts />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
