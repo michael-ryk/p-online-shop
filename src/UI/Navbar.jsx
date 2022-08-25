8ba3552e7e45704585bc6465b624a484eee48b9d
MichaelR <spider747@gmail.com>
2022-08-25 21:08:59 +0300

    Fill Navbar component

--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -1,6 +1,103 @@
-const Navbar = () => {
+import { Link, NavLink } from 'react-router-dom';
 
-  return <h1>Navbar</h1>
-}
+import styled from 'styled-components';
+
+//NavStyles - nav container
+
+const Navbar = () => {
+  return (
+    <NavStyles>
+      <div className='nav-main'>
+        <div className='nav-header'>
+          <Link to='/'>
+            <h1>LOGO</h1>
+          </Link>
+          <button type='button' className='nav-btn'>
+            Burger
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <NavLink to='/homepage'>עמוד הבית</NavLink>
+          </li>
+          <li>
+            <NavLink to='/products'>מוצרים</NavLink>
+          </li>
+          <li>
+            <NavLink to='/about'>עלינו</NavLink>
+          </li>
+          <li>
+            <NavLink to='/contacts'>צור קשר</NavLink>
+          </li>
+        </ul>
+      </div>
+    </NavStyles>
+  );
+};
+
+const NavStyles = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5rem;
+
+  .nav-main {
+    width: 95vw;
+    margin: 0 auto;
+    max-width: var(--max-width);
+  }
+
+  .nav-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .nav-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--clr-primary-5);
+    cursor: pointer;
+  }
+
+  .nav-links {
+    display: none;
+  }
+
+  @media only screen and ( min-width: 990px ){
+    .nav-btn {
+      /* display: none; In future will be hidden */ 
+    }
+
+    .nav-main {
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+      align-items: center;
+    }
+
+    .nav-links {
+      display: flex;
+      justify-content: center;
+      flex-direction: row-reverse;
+      margin: 0.5rem 0;
+
+      li {
+        color: var(--clr-heading-3);
+        margin: 0 1rem;
+        font-size: 1.6rem;
+        letter-spacing: var(--spacing);
+        padding: 0 0.5rem;
+        border-radius: 10px;
+
+        &:hover{
+          color: var(--clr-primary-1);
+          background-color: var(--clr-primary-10);
+          font-weight: bold;
+        }
+      }
+    }
+  }
+
+`;
 
 export default Navbar;
