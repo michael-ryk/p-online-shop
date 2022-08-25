c9f2b700e4e0165c2d59f8a1f5b0b41efb175baf
MichaelR <spider747@gmail.com>
2022-08-25 21:09:35 +0300

    Add global styles to index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+import './index.css';   //All general styles + variables
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
