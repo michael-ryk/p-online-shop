9eeaa7247f2375355f8acf757cfb220d61b52bd4
MichaelR <spider747@gmail.com>
2022-08-27 19:21:22 +0300

    Fill Footer

--- src/UI/Footer.jsx
+++ src/UI/Footer.jsx
@@ -1,6 +1,35 @@
-const Footer = () => {
+import styled from 'styled-components';
 
-  return <h1>Footer</h1>
-}
+const Footer = () => {
+  return (
+    <FooterStyle>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span> Online shop</span>
+      </h5>
+      <p>All rights preserved</p>
+    </FooterStyle>
+  );
+};
+
+const FooterStyle = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  height: 5rem;
+  background-color: var(--clr-heading-4);
+
+  span {
+    color: var(--clr-primary-5);
+  }
+
+  h5,
+  p {
+    color: var(--clr-paragraph-8);
+    font-weight: bold;
+    margin: 0.5rem;
+  }
+`;
 
 export default Footer;
