0c0e5c565f0e8058d8a5d5e3ecaf7cc82bc4f925
MichaelR <spider747@gmail.com>
2022-08-28 20:22:07 +0300

    Fill Not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,38 @@
-const NotFound = () => {
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-  return <h1>NotFound</h1>
-}
+const NotFound = () => {
+  return (
+    <ErrorStyles>
+      <h2>Sorry... but page you search is not available</h2>
+      <button>Go Home</button>
+    </ErrorStyles>
+  );
+};
+
+const ErrorStyles = styled.section`
+  padding: 5rem 0;
+  margin: 0 auto;
+  background-color: var(--clr-primary-10);
+  width: 95vw;
+  max-width: var(--max-width);
+  h2 {
+    text-align: center;
+  }
+  button {
+    display: block;
+    margin: 1rem auto;
+    background: transparent;
+    border-color: transparent;
+    font-size: 1.7rem;
+    cursor: pointer;
+    letter-spacing: var(--spacing);
+    border: 0;
+  }
+
+  button:hover {
+    color: var(--clr-green-dark);
+  }
+`;
 
 export default NotFound;
