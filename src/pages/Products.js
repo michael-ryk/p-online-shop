82eefbf9dd0e0b0201a8863e8c1d4069cc73f62e
MichaelR <spider747@gmail.com>
2022-08-30 20:11:35 +0300

    Add first 2 products from dummy list

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,4 +1,15 @@
+import { useState, useEffect } from 'react';
+import dummyList from '../constants/dummyList';
+
 const Products = () => {
+
+  const [ products, setProducts ] = useState([]);
+
+  // simulate fetch from backend
+  useEffect( () => {
+    setProducts(dummyList);
+  },[]);
+
 
   return <h1>Products</h1>
 }
