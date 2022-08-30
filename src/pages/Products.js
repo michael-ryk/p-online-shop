942f0939c10b951f3e153665757b9857f73cbf28
MichaelR <spider747@gmail.com>
2022-08-30 20:53:26 +0300

    Add product component - show products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
+
+import Product from '../components/Product';
 import dummyList from '../constants/dummyList';
+
+import styled from 'styled-components';
 
 const Products = () => {
 
@@ -11,7 +15,32 @@ const Products = () => {
   },[]);
 
 
-  return <h1>Products</h1>
+  return (
+    <ProductsStyles>
+      {products.map( item => {
+        return (
+          <Product key={item.id} {...item} />
+        )
+      })}
+    </ProductsStyles>
+  );
 }
+
+const ProductsStyles = styled.div`
+  max-width: var(--max-width);
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.5rem 0.5rem;
+
+  
+  @media only screen and (max-width: 950px){
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media only screen and (max-width: 630px){
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
 
 export default Products;
