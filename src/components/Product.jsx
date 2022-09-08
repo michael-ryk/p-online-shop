0ee40b726e439f417020cd7626095b774a24ce63
MichaelR <spider747@gmail.com>
2022-09-08 22:46:32 +0300

    Fix/Product paragraph escape

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,42 +4,34 @@ import styled from 'styled-components';
 const Product = ({name, price, image, description, category}) => {
   return (
     <ProductStyle>
-      <div className='product-container'>
-        <img src={image} alt='preview' />
-        <h3>{name}</h3>
-        <h2>{formatPrice(price)}</h2>
-        <p>{description}</p>
-      </div>
+      <img src={image} alt='preview' />
+      <h3>{name}</h3>
+      <h2>{formatPrice(price)}</h2>
+      <p>{description}</p>
     </ProductStyle>
   )
 }
 
 const ProductStyle = styled.div`
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: column;
   text-align: right;
+  max-width: 300px;
   margin: 0.5rem auto;
   padding: 0.5rem;
-  
-
-  .product-container{
-    h3 {
-      color: var(--clr-heading-2);
-    }
-    h2 {
-      color: var(--clr-paragraph-6);
-    }
+  h3 {
+    color: var(--clr-heading-2);
   }
-  .product-container img{
-    max-width: 300px;
+  h2 {
+    color: var(--clr-paragraph-6);
+  }
+  img{
+    padding: 1rem;
     &:hover{
       transition: var(--transition);
       transform: scale(105%);
     }
-
   }
-
-
 `
 
 export default Product;
