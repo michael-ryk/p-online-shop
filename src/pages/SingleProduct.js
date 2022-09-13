77c4cebbff4634438cfc8e8d18326e225e4f59a6
MichaelR <spider747@gmail.com>
2022-09-13 20:11:56 +0300

    fetch single products on single product page

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,6 +1,45 @@
-const SingleProduct = () => {
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { singleProduct_url } from '../constants/urls';
+import styled from 'styled-components';
 
-  return <h1>SingleProduct</h1>
+const SingleProduct = () => {
+  const {id} = useParams();
+  const [singleProduct, setSingleProduct] = useState();
+  
+  useEffect(() => {
+    const fetchSingleProduct = async () => {
+      const response = await fetch(singleProduct_url);
+      const data = await response.json();
+
+      if(!response.ok){
+        throw new Error('Problem connect to DB!')
+      }
+      
+      console.log(data[1])
+      setSingleProduct(data[1]);
+      console.log(singleProduct)
+      const {name, price, category} = singleProduct;
+    }
+
+    fetchSingleProduct().catch(err => {
+      console.log(err.message);
+    })
+
+  }, [id]);
+
+  
+  
+  return (
+    <SingleProductStyled>
+      <h1>vsdf</h1>
+      <p></p>
+    </SingleProductStyled>
+  );
 }
+
+const SingleProductStyled = styled.main`
+
+`
 
 export default SingleProduct;
