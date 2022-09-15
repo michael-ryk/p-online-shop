e9429ff48659037293e6bf0b591e68bd04452245
MichaelR <spider747@gmail.com>
2022-09-15 23:00:30 +0300

    Products page - Take product list from store instead of fetch

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,56 +1,20 @@
-import { useState, useEffect } from 'react';
-import { products_url } from '../constants/urls';
+import { useSelector } from 'react-redux';
 import Product from '../components/Product';
 
 import styled from 'styled-components';
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
 
-  // simulate fetch from backend
-  useEffect(() => {
+  // Take list of products from Redux store
+  const products = useSelector(state => state.productsList)
 
-    // fetch using async await
-    
-    const fetchProductsList = async () => {
-      const response = await fetch(products_url);
-      const data = await response.json();
-
-      if(!response.ok){
-        throw new Error('Problem connect to DB!')
-      }
-
-      // Adapt data to be handled by map()
-      const adaptedProductsList = []
-      for (const key in data) {
-        if(data[key]) {
-          adaptedProductsList.push({
-            id: key,
-            name: data[key].name,
-            price: data[key].price,
-            description: data[key].description,
-            image: data[key].image,
-            category: data[key].category,
-          });
-        }
-      }
-
-      setProducts(adaptedProductsList);
-    };
-
-    // trigger async function to fetch data and catch errors
-    fetchProductsList().catch(err => {
-      console.log(err.message);
-    });
-
-    
-  }, []);
 
   return (
     <ProductsStyles>
-      {products.map((item) => {
+      {products && products.map((item) => {
         return <Product key={item.id} {...item} />;
       })}
+      {!products && <p>Product list is empty</p>}
     </ProductsStyles>
   );
 };
