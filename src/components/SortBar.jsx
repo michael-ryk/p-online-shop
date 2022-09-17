05b5cb863f084a7ad7be5a3b132009bbaedc7ef2
MichaelR <spider747@gmail.com>
2022-09-17 19:42:15 +0300

    Create Sorting bar component with styles

--- src/components/SortBar.jsx
+++ src/components/SortBar.jsx
@@ -1,15 +1,73 @@
 import styled from 'styled-components';
+import { BsGridFill, BsList } from 'react-icons/bs';
 
 const SortBar = () => {
   return (
     <SortBarStyles>
-      <h1>Sorting Bar</h1>
+      <div className='btn-container'>
+        <button>
+          <BsGridFill />
+        </button>
+        <button>
+          <BsList />
+        </button>
+      </div>
+      <p>נמצאו 300 מוצרים</p>
+      <form>
+        <label htmlFor='sort'>מיון לפי</label>
+        <select name='sort' id='sort' className='sort-input'>
+          <option value='price-lowest'>מחיר (נמוך לגבוהה)</option>
+          <option value='price-highers'>מחיר (גבוהה לנמוך)</option>
+          <option value='name-a'>שם הפריט (א-ת)</option>
+          <option value='name-z'>שם הפריט (ת-א)</option>
+        </select>
+      </form>
     </SortBarStyles>
-  )
-}
+  );
+};
 
 const SortBarStyles = styled.section`
-  text-align: left;
-`
+  margin-bottom: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media only screen and (max-width: 620px) {
+    flex-direction: column;
+  }
+  .btn-container {
+    margin: 0 0.5rem;
+    button {
+      margin: 0 0.25rem;
+      padding: 0 0.5rem;
+      background: transparent;
+      border: 1px solid var(--clr-heading-3);
+      border-radius: var(--radius);
+      cursor: pointer;
+      color: var(--clr-heading-3);
+      &:hover{
+        transition: var(--transition);
+        color: var(--clr-paragraph-5);
+      }
+      svg {
+        font-size: 2rem;
+      }
+    }
+  }
+  p {
+    font-size: 1.25rem;
+    margin: 0 2rem;
+  }
+  form {
+    .sort-input{
+      border-radius: var(--radius);
+      font-size: 1.25rem;
+      padding: 0.25rem 0.5rem;
+      margin-right: 0.5rem;
+    }
+    label {
+      font-size: 1.25rem;
+    }
+  }
+`;
 
 export default SortBar;
