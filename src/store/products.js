09e06c37e9c773a307e40ae3e51fe21f3cedf4d2
MichaelR <spider747@gmail.com>
2022-09-20 11:26:27 +0300

    Add sidebar close open reducers

--- src/store/products.js
+++ src/store/products.js
@@ -2,15 +2,22 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   productsList: [],
+  showSidebar: false,
 }
 
 const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
+    openSidebar(state){
+      state.showSidebar = true;
+    },
+    closeSidebar(state){
+      state.showSidebar = false;
+    },
     addProductsToStore(state, action){
       state.productsList = action.payload.productsList;
-    }
+    },
   }
 });
 
