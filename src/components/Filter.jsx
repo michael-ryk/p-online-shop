c4cb2fd4b5f6bac3d33e11515218eb6a1caf18b2
MichaelR <spider747@gmail.com>
2022-09-24 16:26:52 +0300

    add filter component

---
+++ src/components/Filter.jsx
@@ -0,0 +1,7 @@
+const Filter = () => {
+  return (
+    <h1>Filter and search</h1>
+  )
+}
+
+export default Filter;
--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,20 +1,32 @@
 import ProductsList from '../components/ProductsList';
 import SortBar from '../components/SortBar';
+import Filter from '../components/Filter';
 import styled from 'styled-components';
 
 const ProductsPage = () => {
 
   return (
-    <ProductsPageStyles>
-      <SortBar />
-      <ProductsList />
-    </ProductsPageStyles>
+    <main>
+      <ProductsPageStyles>
+        <Filter />
+        <div>
+          <SortBar />
+          <ProductsList />
+        </div>
+      </ProductsPageStyles>
+    </main>
   );
 };
 
 const ProductsPageStyles = styled.div`
   max-width: var(--max-width);
   margin: 0 auto;
+  display: flex;
+
+  @media only screen and (max-width: 450px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export default ProductsPage;
