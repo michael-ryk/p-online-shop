000f2f15e2831ff23cc361308baf147561db22ce
MichaelR <spider747@gmail.com>
2022-09-24 18:23:28 +0300

    add visual elements of filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,65 @@
+import styled from 'styled-components';
+
 const Filter = () => {
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <h1>Filter and search</h1>
+    <FilterStyles>
+      <form onSubmit={submitHandler}>
+        <div className='form-block'>
+          <input type='text' placeholder='Search' className='search-input'></input>
+        </div>
+        <div className='form-block'>
+          <h3>קטגוריה</h3>
+        </div>
+        <div className='form-block'>
+          <h3>צבע</h3>
+        </div>
+        <div className='form-block'>
+          <h3>מחיר</h3>
+        </div>
+        <div className='form-block'>
+          <h3>מבצעים</h3>
+        </div>
+      </form>
+      <button type='button' className='clear-btn'>אפס סינון</button>
+    </FilterStyles>
   )
 }
+
+const FilterStyles = styled.section`
+  padding: 0.5rem;
+
+  @media only screen and (max-width: 450px){
+    text-align: center;
+  }
+
+  .form-block{
+    margin: 1rem auto;
+  }
+  .search-input{
+    border-radius: var(--radius);
+    font-size: 1.5rem;
+    padding: 0.25rem 0.5rem;
+  }
+  .clear-btn {
+    display: block;
+    width: 100%;
+    font-size: 20px;
+    border: 1px solid transparent;
+    border-radius: var(--radius);
+    background-color: var(--clr-paragraph-9);
+    padding: 0.5rem 1rem;
+    &:hover {
+      background-color: var(--clr-paragraph-8);
+    }
+    &:active {
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
+    }
+  }
+`
 
 export default Filter;
