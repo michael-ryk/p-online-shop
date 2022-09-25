933b702c410331ec596c7c77b4352abae01d5209
MichaelR <spider747@gmail.com>
2022-09-25 17:52:34 +0300

    add filter state

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,16 +1,31 @@
+import { useDispatch } from 'react-redux';
+import { filterActions } from '../store/filter';
 import styled from 'styled-components';
 
 const Filter = () => {
 
+  const dispatch = useDispatch()
+
   const submitHandler = (e) => {
     e.preventDefault();
+  };
+
+  const handleFilterChanges = (e) => {
+    const { name, value } = e.target;
+    dispatch(filterActions.updateFilters({name, value}))
   }
 
   return (
     <FilterStyles>
       <form onSubmit={submitHandler}>
         <div className='form-block'>
-          <input type='text' placeholder='Search' className='search-input'></input>
+          <input
+            name='search_text'
+            type='text'
+            placeholder='Search'
+            className='search-input'
+            onChange={handleFilterChanges}
+          />
         </div>
         <div className='form-block'>
           <h3>קטגוריה</h3>
@@ -25,22 +40,24 @@ const Filter = () => {
           <h3>מבצעים</h3>
         </div>
       </form>
-      <button type='button' className='clear-btn'>אפס סינון</button>
+      <button type='button' className='clear-btn'>
+        אפס סינון
+      </button>
     </FilterStyles>
-  )
-}
+  );
+};
 
 const FilterStyles = styled.section`
   padding: 0.5rem;
 
-  @media only screen and (max-width: 450px){
+  @media only screen and (max-width: 450px) {
     text-align: center;
   }
 
-  .form-block{
+  .form-block {
     margin: 1rem auto;
   }
-  .search-input{
+  .search-input {
     border-radius: var(--radius);
     font-size: 1.5rem;
     padding: 0.25rem 0.5rem;
@@ -60,6 +77,6 @@ const FilterStyles = styled.section`
       box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
     }
   }
-`
+`;
 
 export default Filter;
--- src/store/filter.js
+++ src/store/filter.js
@@ -6,16 +6,25 @@ const initialState = {
   NumberOfFilteredProducts: 0,
   viewType: 'grid',
   sortValue: 'price-lowest',
-}
+  filters: {
+    search_text: '',
+    category: 'all',
+    color: 'all',
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    discount: false,
+  },
+};
 
 const filterSlice = createSlice({
   name: 'filter',
   initialState,
   reducers: {
-    setViewMode(state, action){
+    setViewMode(state, action) {
       state.viewType = action.payload;
     },
-    addProductsToStoreFirstTime(state, action){
+    addProductsToStoreFirstTime(state, action) {
       // console.log(':: Reducer - addProductsToStoreFirstTime')
       state.fetchedProducts = action.payload;
       state.filteredProducts = action.payload;
@@ -23,10 +32,10 @@ const filterSlice = createSlice({
       // console.log('fetchedProducts = ')
       // console.log(state.fetchedProducts)
     },
-    editSortValue(state, action){
+    editSortValue(state, action) {
       state.sortValue = action.payload;
     },
-    sortProducts(state){
+    sortProducts(state) {
       // console.log(':: Reducer - sortProducts')
       let tmpProducts = [...current(state.filteredProducts)];
       // console.log('Current state of filteredProducts: ')
@@ -34,34 +43,47 @@ const filterSlice = createSlice({
       switch (state.sortValue) {
         case 'price-lowest':
           // console.log('If else - price-lowest');
-          state.filteredProducts = tmpProducts.sort((a,b) => {
+          state.filteredProducts = tmpProducts.sort((a, b) => {
             if (a.price < b.price) {
-              return -1
+              return -1;
             }
             if (a.price > b.price) {
-              return 1
+              return 1;
             }
-            return 0
+            return 0;
           });
           break;
         case 'price-highers':
           // console.log('If else - price-highers');
-          state.filteredProducts = tmpProducts.sort((a,b) => b.price - a.price);
+          state.filteredProducts = tmpProducts.sort(
+            (a, b) => b.price - a.price
+          );
           break;
         case 'name-a':
           // console.log('If else - name-a');
-          state.filteredProducts = tmpProducts.sort((a,b) => a.name.localeCompare(b.name));
+          state.filteredProducts = tmpProducts.sort((a, b) =>
+            a.name.localeCompare(b.name)
+          );
           break;
         case 'name-z':
           // console.log('If else - name-z');
-          state.filteredProducts = tmpProducts.sort((a,b) => b.name.localeCompare(a.name));
+          state.filteredProducts = tmpProducts.sort((a, b) =>
+            b.name.localeCompare(a.name)
+          );
           break;
         default:
           console.log('Switch - no match found');
           break;
       }
-      },
+    },
+    updateFilters(state, action) {
+      state.filters.search_text = action.payload.value;
+      console.log(state.filters.search_text);
+    },
+    filterProducts(state) {
+
     }
+  },
 });
 
 export const filterActions = filterSlice.actions;
